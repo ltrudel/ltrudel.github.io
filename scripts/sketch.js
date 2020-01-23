2e46eec0136b9e44e42f833a115bd21497613474
Lance <trudellance@gmail.com>
2020-01-23 12:39:30 -0600

    1

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,40 +1,8 @@
-/// <reference path="./p5.global-mode.d.ts" />
-
 function setup(){
     createCanvas(windowWidth,windowHeight);
 }
 function draw(){
-
-    background(200);
-    /*
-    strokeWeight(0);
-    stroke(255)
-    fill(0, 0, 100);
-    rect(00,00,windowWidth/2,windowHeight/2);
-
-    fill(0, 0, 100);
-    rect(windowWidth/2,windowHeight/2, windowWidth, windowHeight);
-
-    strokeWeight(200);
-    stroke(200 ,40, 40);
-    line(windowWidth/2, 0, windowWidth/2, windowHeight);
-    line(0, windowHeight/2, windowWidth, windowHeight/2);
-    line(0, 0, windowWidth, windowHeight);
-    line(windowWidth, 0, 0, windowHeight);
-
-    strokeWeight(0);
-    fill(255);
-    beginShape();
-    vertex(windowWidth/2, windowHeight/2-windowHeight/4);
-    vertex(windowWidth/2+windowWidth/4, windowHeight/2);
-    vertex(windowWidth/2, windowHeight/2+windowHeight/4);
-    vertex(windowWidth/2-windowWidth/4, windowHeight/2);
-    endShape(CLOSE);
-
-    fill(200, 40, 40);
-    ellipse(windowWidth/2, windowHeight/2, 300, 300);
-    */
-
+    
     var size = 100;
 
     for (let i = -2; i < windowWidth/size; i++) {
