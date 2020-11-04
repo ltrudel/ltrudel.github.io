69408e753eff593213a7e55063ff29b8731323ba
lance-t <ltrudel@monusb.ca>
2020-11-04 15:48:24 -0600

    compte-rendu-temp

---
+++ script.js
@@ -0,0 +1,95 @@
+
+  //    The function evaluates the answer and displays result
+  function displayAnswer1() {
+    if (document.getElementById('option-1').checked) {
+      document.getElementById('block-1').style.border = '3px solid limegreen'
+      document.getElementById('result-1').style.color = 'limegreen'
+      document.getElementById('result-1').innerHTML = 'Correcte!'
+    }
+    if (document.getElementById('option-2').checked) {
+      document.getElementById('block-2').style.border = '3px solid red'
+      document.getElementById('result-2').style.color = 'red'
+      document.getElementById('result-2').innerHTML = 'Incorrecte!'
+      showCorrectAnswer1()
+    }
+    if (document.getElementById('option-3').checked) {
+      document.getElementById('block-3').style.border = '3px solid red'
+      document.getElementById('result-3').style.color = 'red'
+      document.getElementById('result-3').innerHTML = 'Incorrecte!'
+      showCorrectAnswer1()
+    }
+    if (document.getElementById('option-4').checked) {
+      document.getElementById('block-4').style.border = '3px solid red'
+      document.getElementById('result-4').style.color = 'red'
+      document.getElementById('result-4').innerHTML = 'Incorrecte!'
+      showCorrectAnswer1()
+    }
+  }
+
+  
+  // the functon displays the link to the correct answer
+  function showCorrectAnswer1() {
+  }
+  //    The function evaluates the answer and displays result
+  function displayAnswer2() {
+    if (document.getElementById('option-21').checked) {
+      document.getElementById('block-21').style.border = '3px solid red'
+      document.getElementById('result-21').style.color = 'red'
+      document.getElementById('result-21').innerHTML = 'Incorrecte!'
+    }
+    if (document.getElementById('option-22').checked) {
+      document.getElementById('block-22').style.border = '3px solid limegreen'
+      document.getElementById('result-22').style.color = 'limegreen'
+      document.getElementById('result-22').innerHTML = 'Correcte!'
+      showCorrectAnswer2()
+    }
+    if (document.getElementById('option-23').checked) {
+      document.getElementById('block-23').style.border = '3px solid red'
+      document.getElementById('result-23').style.color = 'red'
+      document.getElementById('result-23').innerHTML = 'Incorrecte!'
+      showCorrectAnswer2()
+    }
+    if (document.getElementById('option-24').checked) {
+      document.getElementById('block-24').style.border = '3px solid limegreen'
+      document.getElementById('result-24').style.color = 'limegreen'
+      document.getElementById('result-24').innerHTML = 'Correcte!'
+      showCorrectAnswer2()
+    }
+  }
+
+  
+  // the functon displays the link to the correct answer
+  function showCorrectAnswer2() {
+  }
+  //    The function evaluates the answer and displays result
+  function displayAnswer3() {
+    if (document.getElementById('option-31').checked) {
+      document.getElementById('block-31').style.border = '3px solid red'
+      document.getElementById('result-31').style.color = 'red'
+      document.getElementById('result-31').innerHTML = 'Incorrecte!'
+    }
+    if (document.getElementById('option-32').checked) {
+      document.getElementById('block-32').style.border = '3px solid limegreen'
+      document.getElementById('result-32').style.color = 'limegreen'
+      document.getElementById('result-32').innerHTML = 'Correcte!'
+      showCorrectAnswer3()
+    }
+    if (document.getElementById('option33').checked) {
+      document.getElementById('block-33').style.border = '3px solid red'
+      document.getElementById('result-33').style.color = 'red'
+      document.getElementById('result-33').innerHTML = 'Incorrecte!'
+      showCorrectAnswer3()
+    }
+    if (document.getElementById('option-34').checked) {
+      document.getElementById('block-34').style.border = '3px solid red'
+      document.getElementById('result-34').style.color = 'red'
+      document.getElementById('result-34').innerHTML = 'Incorrecte!'
+      showCorrectAnswer3()
+    }
+  }
+
+  
+  // the functon displays the link to the correct answer
+  function showCorrectAnswer3() {
+
+  }
